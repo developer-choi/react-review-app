54b80b71203de4749ea804fe2153b95e2b27c970
Yu Jin Choe <forworkchoe@gmail.com>
2022-05-07 22:17:27 +0900

    Add Clock component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
-
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
-
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
+import {useEffect, useState} from 'react';
 
 function App() {
-  return element;
+  
+  const [current, setCurrent] = useState(() => new Date());
+  
+  useEffect(() => {
+    console.log('render');
+  });
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setCurrent(() => new Date());
+    }, 1000);
+  }, [current]);
+  
+  return (
+    <h1>
+      {current.toLocaleTimeString()}
+    </h1>
+  );
 }
 
 export default App;
