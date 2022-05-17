61e8ccf454c277fcba149f55517a3a29ff3a2682
Yu Jin Choe <forworkchoe@gmail.com>
2022-05-17 21:33:54 +0900

    Add the example of useMemo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React, {useCallback, useRef} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 
 function App() {
   
-  const inputRef = useRef(null);
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(100);
+  const [c, setC] = useState(10000);
   
-  const handleClick = useCallback(() => {
-    inputRef.current.focus();
+  const memoizedValue = useMemo(() => {
+    console.log('Called the useMemo callback');
+    return a + b;
+  }, [a, b]);
+  
+  const increaseA = useCallback(() => {
+    setA(prevState => prevState + 1);
+  }, []);
+  
+  const decreaseB = useCallback(() => {
+    setB(prevState => prevState - 1);
+  }, []);
+  
+  const decreaseC = useCallback(() => {
+    setC(prevState => prevState - 100);
   }, []);
   
   return (
     <>
-      <input ref={inputRef} placeholder="Please enter your name"/>
-      <button onClick={handleClick}>Force the input</button>
+      <h2>a = {a}</h2>
+      <h2>b = {b}</h2>
+      <h2>c = {c}</h2>
+      <h2>memoizedValue = {memoizedValue}</h2>
+      <button onClick={increaseA}>Increase A</button>
+      <button onClick={decreaseB}>decrease B</button>
+      <button onClick={decreaseC}>decrease C</button>
     </>
   );
 }
