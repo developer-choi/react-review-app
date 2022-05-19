2a951bf13e5876211961537472d221274c67e8b5
Yu Jin Choe <forworkchoe@gmail.com>
2022-05-19 22:56:17 +0900

    Add the example of useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,24 @@
-import React, {useCallback, useMemo, useState} from 'react';
+import React, {useReducer} from 'react';
+
+function reducer(prevState: {count: number}, action: { type: 'increase' | 'decrease' }) {
+  switch (action.type) {
+    case 'decrease':
+      return {count: prevState.count - 1};
+    case 'increase':
+      return {count: prevState.count + 1};
+    default:
+      return prevState;
+  }
+}
 
 function App() {
-  
-  const [a, setA] = useState(0);
-  const [b, setB] = useState(100);
-  const [c, setC] = useState(10000);
-  
-  const memoizedValue = useMemo(() => {
-    console.log('Called the useMemo callback');
-    return a + b;
-  }, [a, b]);
-  
-  const increaseA = useCallback(() => {
-    setA(prevState => prevState + 1);
-  }, []);
-  
-  const decreaseB = useCallback(() => {
-    setB(prevState => prevState - 1);
-  }, []);
-  
-  const decreaseC = useCallback(() => {
-    setC(prevState => prevState - 100);
-  }, []);
+  const [state, dispatch] = useReducer(reducer, {count: 0});
   
   return (
     <>
-      <h2>a = {a}</h2>
-      <h2>b = {b}</h2>
-      <h2>c = {c}</h2>
-      <h2>memoizedValue = {memoizedValue}</h2>
-      <button onClick={increaseA}>Increase A</button>
-      <button onClick={decreaseB}>decrease B</button>
-      <button onClick={decreaseC}>decrease C</button>
+      <h2>value = {state.count}</h2>
+      <button onClick={() => dispatch({type: 'increase'})}>Increase</button>
+      <button onClick={() => dispatch({type: 'decrease'})}>decrease</button>
     </>
   );
 }
